17b0f800b22c7210104194535c0956781619304d
Gustavo Rossy <gustavorossy@gmail.com>
2022-09-11 21:52:08 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import model.entities.Department;
+
 public class Program {
     public static void main(String[] args){
 
-        
+        Department d = new Department(1, "Sellers");
+
+        System.out.println(d);
 
     }
 }
